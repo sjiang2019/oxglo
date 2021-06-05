79026020cfa649184bc913c61978c82a058f57cd
Steven Jiang <sj9797@gmail.com>
2021-06-04 21:08:49 -0700

    fixed onboarding

--- App.tsx
+++ App.tsx
@@ -10,11 +10,8 @@ import {
   useAuthState,
 } from './src/utils/AuthUtils';
 import StartNavigator from './src/navigation/StartNavigator';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavScreen, SCREEN_TO_NAME} from './src/constants/NavConstants';
 
 export const AuthContext = React.createContext();
-const Stack = createStackNavigator();
 
 const AppStack = () => {
   const [state, dispatch] = React.useReducer(useAuthState, DEFAULT_AUTH_STATE);
@@ -24,25 +21,10 @@ const AppStack = () => {
   }, []);
 
   const authContext = React.useMemo(() => useAuthContextMemo(dispatch), []);
-  const initialRouteName =
-    state.userToken == null
-      ? SCREEN_TO_NAME[NavScreen.StartNavigator]
-      : SCREEN_TO_NAME[NavScreen.AppNavigator];
   return (
     <AuthContext.Provider value={authContext}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={initialRouteName}>
-          <Stack.Screen
-            name={SCREEN_TO_NAME[NavScreen.AppNavigator]}
-            component={AppNavigator}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name={SCREEN_TO_NAME[NavScreen.StartNavigator]}
-            component={StartNavigator}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
+        {state.userToken == null ? <StartNavigator /> : <AppNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
   );
--- src/screens/start/SignInScreen.tsx
+++ src/screens/start/SignInScreen.tsx
@@ -3,10 +3,9 @@ import * as React from 'react';
 import {AuthContext} from '../../../App';
 import SignInView from './SignInScreenView';
 
-export default function SignInScreen() {
+export default function SignInScreen({navigation}) {
   const {signIn} = React.useContext(AuthContext);
   // USE ELSEWHERE
   const {signOut} = React.useContext(AuthContext);
-
   return <SignInView onClickSignIn={signIn} />;
 }
